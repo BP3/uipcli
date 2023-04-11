41d2d81b86779e8ca76a04fd4ed7eb4c5f97d2f7
Justin Phillips <jphillips@bp-3.com>
2023-04-11 15:42:31 +0100

    Extra shift for shell check script that is passed in

--- uipcli.sh
+++ uipcli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" = "sh" -a "$2" = "-c" ]; then
-  shift 2
+  shift 3
 elif [ "$1" = "bash" -a "$2" = "-c" ]; then
-  shift 2
+  shift 3
 fi
 
 exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
