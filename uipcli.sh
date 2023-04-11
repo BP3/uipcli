d9a97928330c4729b5670fde71fd301cc27faa57
Justin Phillips <jphillips@bp-3.com>
2023-04-11 11:28:06 +0100

    Added debug

--- uipcli.sh
+++ uipcli.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [[ -n "$CI" ]]; then
+  echo "Running inside GL pipeline"
   exec /bin/bash
 else
+  echo "Running locally"
   exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
 fi
