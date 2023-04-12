dcd955536ba69db909cab1d5903bda225c4a0634
Justin Phillips <jphillips@bp-3.com>
2023-04-12 13:30:53 +0100

    Reverted back to having to specify the exact command for running in GL

--- uipcli.sh
+++ uipcli.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-#echo "Before shifting by 3"
-#echo "$@"
-#
-#if [ "$1" = "sh" -a "$2" = "-c" ]; then
-#  shift 3
-#elif [ "$1" = "bash" -a "$2" = "-c" ]; then
-#  shift 3
-#fi
-#
-#echo "After shifting by 3"
-#echo "$@"
-#
-#exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
+# This script has to handle being called locally or from a GitLab CI / CD pipeline.
+# The requirements are stipulated here (although provides no real examples):
+#   https://docs.gitlab.com/runner/executors/docker.html#configure-a-docker-entrypoint
+# Further more, this link shows an example based on those requirements which this script is based upon:
+#   https://stackoverflow.com/questions/70401876/gitlab-runner-doesnt-run-entrypoint-scripts-in-dockerfile
 
+# The CI variable is boolean that is set to true of we are running in a GitLab or GitHub CI / CD environment
+# so we execute a bash shell where the script block commands are piped into. Note that the 'CI' variable
+# is a standard convention used for many CI / CD environments so this should be compatible.
 if [[ -n "$CI" ]]; then
-  echo "Params when running inside GL"
-  echo "$@"
-
-  exec /bin/bash
-# Otherwise we are running out side, so execute the command line wrapper script directly
+    exec /bin/bash
+# Otherwise we are running outside of a CI / CD environment, so execute the command line wrapper script directly
 else
-  echo "Params when running locally"
-  echo "$@"
-
-  exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
+    shift
+    exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
 fi
