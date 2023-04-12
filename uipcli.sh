bc3e1a7b6902b52007fa29d43e1238e315baeb73
Justin Phillips <jphillips@bp-3.com>
2023-04-12 11:31:55 +0100

    Added CI var check and shell exec

--- uipcli.sh
+++ uipcli.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-echo "Before shifting by 3"
-echo "$@"
+#echo "Before shifting by 3"
+#echo "$@"
+#
+#if [ "$1" = "sh" -a "$2" = "-c" ]; then
+#  shift 3
+#elif [ "$1" = "bash" -a "$2" = "-c" ]; then
+#  shift 3
+#fi
+#
+#echo "After shifting by 3"
+#echo "$@"
+#
+#exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
 
-if [ "$1" = "sh" -a "$2" = "-c" ]; then
-  shift 3
-elif [ "$1" = "bash" -a "$2" = "-c" ]; then
-  shift 3
+if [[ -n "$CI" ]]; then
+  echo "Params when running inside GL"
+  echo "$@"
+
+  exec /bin/bash
+# Otherwise we are running out side, so execute the command line wrapper script directly
+else
+  echo "Params when running locally"
+  echo "$@"
+
+  exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
 fi
-
-echo "After shifting by 3"
-echo "$@"
-
-exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
