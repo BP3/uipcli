c72d51b76d3add55661151327131326baa56b1a8
Justin Phillips <jphillips@bp-3.com>
2023-04-12 10:03:43 +0100

    Moved debug

--- uipcli.sh
+++ uipcli.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+echo "Before shifting by 3"
+echo "$1"
+echo "$2"
+echo "$3"
+echo "$@"
+
 if [ "$1" = "sh" -a "$2" = "-c" ]; then
   shift 3
 elif [ "$1" = "bash" -a "$2" = "-c" ]; then
   shift 3
 fi
 
-echo "$1"
-echo "$2"
-echo "$3"
-echo "$@"
 #exec dotnet "$UIPATH_CLI_WORKING_DIR"/uipcli.dll "$@"
